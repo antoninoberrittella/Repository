ad63be5c30e9e3843957ac0d57acd72d16763434
AntonGit361 <128024096+AntonGit361@users.noreply.github.com>
2023-03-16 19:22:22 +0100

    Add files via upload

---
+++ Konto.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+//Header etc fuer Nutzung von strings etc
+class Konto
+{
+private:
+	string customerName;
+	string accountNumber;
+	double balance;
+
+public:
+	void set_Kunde(string Kontoinhaber, string Kontonummer, double Saldo);
+	void deposit(double value);
+	void payoff(double value);
+	const void display();
+	Konto();
+	~Konto();
+};
